3264555df676e8e42b2d083f408b66bfffc2c104
mkorytko <makorytko@yandex.ru>
2018-11-05 23:06:36 +0300

    #2 move start/end propogation

--- src/Slider/Container.js
+++ src/Slider/Container.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-const SliderContainer = ({...props}) => <div className="slider-container">{props.children}</div>;
+const SliderContainer = (props) => <div className="slider-container">{props.children}</div>;
 
 export default SliderContainer;
--- src/Slider/Slider.js
+++ src/Slider/Slider.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
 import SliderContainer from './Container';
+import img_1 from "./assets/1.jpg";
+import img_2 from "./assets/2.jpg";
+import img_3 from "./assets/3.jpg";
+import img_4 from "./assets/4.jpg";
 
 class Slider extends Component {
     state = {
         mark: {
-            start: null,
-            move: null, 
-            direction: null,
-            end: null,
+            start: null, //начало тача
+            move: null, // расстояние от старта тача
+            direction: null, // направление движения -1 - справа налево / 1 - слева направо
+            end: null, // отпускание пальца
+            stop: null, // маркер конца пройденного расстояния move
         }
     }
-
+    
     mark = Object.create(null);
 
     mouseEvent = (e) => {
@@ -19,44 +24,69 @@ class Slider extends Component {
                 this.mark.start = e.touches[0].pageX;
             },
             onTouchMove: e => {
-                this.mark.move = e.touches[0].pageX - this.mark.start;
-                // -1 - справа налево / 1 - слева направо
+                if (!this.mark.stop) {
+                    this.mark.move = e.touches[0].pageX - this.mark.start;    
+                } else {
+                    this.mark.move = this.mark.stop + (e.touches[0].pageX - this.mark.start);
+                }
                 this.mark.direction = (this.mark.start - e.touches[0].pageX) > 0 ? -1 : 1;
-                // this.mark = {
-                //     start: this.mark.start,
-                //     move: this.mark.move,
-                //     direction: this.mark.direction,
-                // }
                 this.setState({
                     mark:{
                         move: this.mark.move,
                     }
-                })
+                });
             },
             onTouchEnd: e => {
                 this.mark.end = e.changedTouches[0].clientX;
+                this.mark.stop = this.mark.move
                 this.mark = {
                     start: this.mark.start,
                     move: this.mark.move,
                     direction: this.mark.direction,
                     end: this.mark.end,
+                    stop: this.mark.move,
                 };
-                this.setState({mark: this.mark})
+                this.setState({mark: this.mark});
             }
         };
     }
-    
+
+    moveTarget = (el) => {
+
+    }
+
 
     render(){
         const mouseEvent = this.mouseEvent();
-        console.log(this.state.mark)
+        //console.log(this.state.mark)
+        const styleIs = {transform: `translateX(${this.state.mark.move}px)`};
         return (
             <React.Fragment>
                 <SliderContainer>
-                    <p {...mouseEvent}
-                    style={{transform: `translateX(${this.mark.move}px)`}}>
-                        Какой-то абзац
-                    </p>
+                    <div className="slides-wrapper" {...mouseEvent}>
+                        <div 
+                        className="slider-box"  style={styleIs}>
+                            <div className="slider-item"></div>
+                            <div className="slider-item"></div>
+                            <div className="slider-item"></div>
+                            <div className="slider-item"></div>
+                            <div className="slider-item">
+                                <img src={img_1} alt="car"/>
+                            </div>
+                            <div className="slider-item">
+                                <img src={img_2} alt="car"/>
+                            </div>
+                            <div className="slider-item">
+                                <img src={img_3} alt="car"/>
+                            </div>
+                            <div className="slider-item">
+                                <img src={img_4} alt="car"/>
+                            </div>
+                            <div className="slider-item"></div>
+                            <div className="slider-item"></div>
+                            <div className="slider-item"></div>
+                        </div>
+                    </div>
                 </SliderContainer>
             </React.Fragment>
         )
